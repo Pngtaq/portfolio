09c0e253168f5d4317a40e813991a4920daa3e46
RaisonSalvador <raisonsalvador0@gmail.com>
2025-09-01 19:58:25 +0800

    fix: fixing and adding skills in skill section

---
+++ components/ui/svg/VercelSVG.tsx
@@ -0,0 +1,7 @@
+export default function VercelSVG() {
+  return (
+    <svg viewBox="0 0 256 256" className="w-16 h-16" xmlns="http://www.w3.org/2000/svg">
+      <path fill="#000" d="M128 0L0 256h256L128 0z"/>
+    </svg>
+  );
+}
